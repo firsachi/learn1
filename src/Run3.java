4b2ea077d5349cb121f2b136b57bc20a584c8dba
firsachi <firsachi.alex@gmail.com>
2017-01-25 12:22:48 +0200

    My first program java

---
+++ src/Run3.java
@@ -0,0 +1,13 @@
+/**
+ * Created by firsov on 25.01.2017.
+ */
+public class Run3 {
+
+    public static void main(String[] args){
+        message();
+    }
+
+     public static void message(){
+         System.out.println("Мой первый класс на Java");
+     }
+}
---
+++ src/Run5.java
@@ -0,0 +1,13 @@
+/**
+ * Created by firsov on 25.01.2017.
+ */
+public class Run5 {
+
+    public static void main(String[] args){
+        message();
+    }
+
+     public static void message(){
+         System.out.println("Мой первый класс на Java");
+     }
+}
