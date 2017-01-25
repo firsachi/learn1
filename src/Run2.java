537686a669ed71952290a8ed553f6f5befd781ac
firsachi <firsachi.alex@gmail.com>
2017-01-25 12:24:28 +0200

    My first program java.

---
+++ src/Run2.java
@@ -0,0 +1,13 @@
+/**
+ * Created by firsov on 25.01.2017.
+ */
+public class Run2 {
+
+    public static void main(String[] args){
+        message();
+    }
+
+     public static void message(){
+         System.out.println("Мой второй класс на Java");
+     }
+}
