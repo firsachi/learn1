f25b651bf1964e25181a7b54486eb497801b61b8
firsachi <firsachi.alex@gmail.com>
2017-03-09 16:11:41 +0200

    create entity room

---
+++ src/task5/entity/Room.java
@@ -0,0 +1,97 @@
+package task5.entity;
+
+import java.util.Date;
+
+/**
+ * Created by firsov on 09.03.2017.
+ */
+public class Room {
+
+    private long id;
+    private int price;
+    private int person;
+    private Date dateAvailableFrom;
+    private String hotelName;
+    private  String cityName;
+
+    public Room(long id, int price, int person, Date dateAvailableFrom, String hotelName, String cityName) {
+        this.id = id;
+        this.price = price;
+        this.person = person;
+        this.dateAvailableFrom = dateAvailableFrom;
+        this.hotelName = hotelName;
+        this.cityName = cityName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getPerson() {
+        return person;
+    }
+
+    public void setPerson(int person) {
+        this.person = person;
+    }
+
+    public Date getDateAvailableFrom() {
+        return dateAvailableFrom;
+    }
+
+    public void setDateAvailableFrom(Date dateAvailableFrom) {
+        this.dateAvailableFrom = dateAvailableFrom;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Room room = (Room) o;
+
+        if (id != room.id) return false;
+        if (price != room.price) return false;
+        if (person != room.person) return false;
+        if (dateAvailableFrom != null ? !dateAvailableFrom.equals(room.dateAvailableFrom) : room.dateAvailableFrom != null)
+            return false;
+        if (hotelName != null ? !hotelName.equals(room.hotelName) : room.hotelName != null) return false;
+        return cityName != null ? cityName.equals(room.cityName) : room.cityName == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = price;
+        result = 31 * result + person;
+        result = 31 * result + (cityName != null ? cityName.hashCode() : 0);
+        return result;
+    }
+}
