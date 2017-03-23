778e40a849619b2574ac9529611412e008e8f4e7
firsachi <macos1q2w3e>
2017-03-23 11:12:31 +0200

    Make sure nobody can inherit your ArraysUtils.

---
+++ src/task6/task_new/ArArraysMain.java
@@ -0,0 +1,25 @@
+package task6.task_new;
+
+import task6.View;
+import task6.utils.ArraysUtils;
+
+/**
+ * Created by firsov on 23.03.2017.
+ */
+public class ArArraysMain {
+
+    public static void main(String[] args) {
+        View view = new View();
+        int array[] = new int[]{-2,-3,-1,-13,-7,-6,-10};
+        view.message("Set array", array);
+        view.message("Sum element", + ArraysUtils.sum(array));
+        view.message("Minimal element array", ArraysUtils.min(array));
+        view.message("Max element array", ArraysUtils.max(array));
+        view.message("Max positive", ArraysUtils.maxPositive(array)) ;
+        view.message("Multiplication array", ArraysUtils.multiplication(array));
+        view.message("Modulus", ArraysUtils.modulus(array));
+        view.message("Second largest element array", ArraysUtils.secondLargest(array));
+        view.message("Reverse array", ArraysUtils.reverse(array));
+        view.message("Search for even elements", ArraysUtils.findEvenElements(array));
+    }
+}
